61a6107a3e2af61358cecc2caaeeaf6802d51210
shadab <shadab.saif@novostack.com>
2022-05-20 13:14:59 +0530

    adding registration stepper

--- app/src/main/java/com/aaonri/com/ui/authentication/register/RegistrationActivity.kt
+++ app/src/main/java/com/aaonri/com/ui/authentication/register/RegistrationActivity.kt
@@ -1,15 +1,22 @@
 package com.aaonri.com.ui.authentication.register
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
 import com.aaonri.com.R
+import com.aceinteract.android.stepper.StepperNavigationView
 
 class RegistrationActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_registration)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.frame_stepper) as NavHostFragment
+        val navController = navHostFragment.navController
 
+        val stepper = findViewById<StepperNavigationView>(R.id.stepper)
+        stepper.setupWithNavController(navController)
 
     }
 }
--- app/src/main/java/com/aaonri/com/ui/authentication/register/fragment/BasicDetailsFragment.kt
+++ app/src/main/java/com/aaonri/com/ui/authentication/register/fragment/BasicDetailsFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.fragment.findNavController
 import com.aaonri.com.R
 
 class BasicDetailsFragment : Fragment() {
@@ -13,6 +15,13 @@ class BasicDetailsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_basic_details, container, false)
+        val view = inflater.inflate(R.layout.fragment_basic_details, container, false)
+
+        val textView = view.findViewById<TextView>(R.id.fragment1)
+        textView.setOnClickListener {
+            findNavController().navigate(R.id.action_basicDetailsFragment_to_addressDetailsFragment)
+        }
+
+        return view
     }
 }
