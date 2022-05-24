89cbadb097f167db90242982a36adfd72ef8322f
shadab <shadab.saif@novostack.com>
2022-05-24 13:51:32 +0530

    design complete for login screen

--- app/src/main/java/com/aaonri/app/ui/authentication/register/fragment/LoginFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/authentication/register/fragment/LoginFragment.kt
@@ -7,15 +7,15 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.aaonri.app.R
-import com.aaonri.app.databinding.FragmentLoginRegisterBinding
+import com.aaonri.app.databinding.FragmentLoginBinding
 
-class LoginRegisterFragment : Fragment() {
-    var introBinding: FragmentLoginRegisterBinding? = null
+class LoginFragment : Fragment() {
+    var introBinding: FragmentLoginBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        introBinding = FragmentLoginRegisterBinding.inflate(inflater, container, false)
+        introBinding = FragmentLoginBinding.inflate(inflater, container, false)
         introBinding?.apply {
             loginBtn.setOnClickListener {
                 findNavController().navigate(R.id.action_introFragment_to_registration_na_graph)
