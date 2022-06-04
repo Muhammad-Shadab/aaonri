7061e81e2db32582a3345675537c998d194218db
shadab <shadab.saif@novostack.com>
2022-06-04 23:19:13 +0530

    adding logout button

--- app/src/main/java/com/aaonri/app/ui/dashboard/HomeScreenFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/dashboard/HomeScreenFragment.kt
@@ -1,20 +1,35 @@
 package com.aaonri.app.ui.dashboard
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.InflateException
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.aaonri.app.MainActivity
 import com.aaonri.app.R
+import com.aaonri.app.databinding.FragmentHomeScreenBinding
+import com.aaonri.app.ui.authentication.AuthActivity
 
 
 class HomeScreenFragment : Fragment() {
-
+    var homeScreenBinding: FragmentHomeScreenBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_screen, container, false)
+        homeScreenBinding = FragmentHomeScreenBinding.inflate(inflater, container, false)
+
+        homeScreenBinding?.apply {
+            logOutBtn.setOnClickListener {
+                val intent = Intent(requireContext(), AuthActivity::class.java)
+                startActivity(intent)
+                activity?.finish()
+            }
+        }
+
+        return homeScreenBinding?.root
     }
 }
