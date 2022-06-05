40f915938eca3fcef2cdafa1dd0a313fdb9ef3e8
shadab <shadab.saif@novostack.com>
2022-06-05 14:35:38 +0530

    email validation completed

--- app/src/main/java/com/aaonri/app/utils/Validator.kt
+++ app/src/main/java/com/aaonri/app/utils/Validator.kt
@@ -4,7 +4,7 @@ class Validator {
     companion object {
 
         fun emailValidation(email: String): Boolean {
-            val emailRegex = "^[A-Za-z](.*)([@]{1})(.{1,})(\\.)(.{1,})"
+            val emailRegex = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$"
             return emailRegex.toRegex().matches(email)
         }
 
