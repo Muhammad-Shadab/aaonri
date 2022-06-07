a11fcf16be8d7057b5ee6221679ed58f330861bb
shadab <shadab.saif@novostack.com>
2022-06-07 20:12:47 +0530

    adding splash screen

--- app/src/main/java/com/aaonri/app/ui/authentication/SplashScreenFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/authentication/SplashScreenFragment.kt
@@ -5,15 +5,30 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.aaonri.app.R
+import com.aaonri.app.databinding.FragmentSplashScreenBinding
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 
 class SplashScreenFragment : Fragment() {
+    var splashScreenBinding: FragmentSplashScreenBinding? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        splashScreenBinding =
+            FragmentSplashScreenBinding.inflate(inflater, container, false)
 
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        var job: Job? = null
+        job = MainScope().launch {
+            delay(2000L)
+            findNavController().navigate(R.id.action_splashScreenFragment_to_loginFragment)
+        }
+
+        return splashScreenBinding?.root
     }
 }
