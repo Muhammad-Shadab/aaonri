36b6f7a5bd86d5708c1c031f7a1a7c68499df5aa
shadab <shadab.saif@novostack.com>
2022-06-07 13:06:32 +0530

    opening gmail app in check your email screen

--- app/src/main/java/com/aaonri/app/ui/authentication/forgot_password/CheckYourEmailFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/authentication/forgot_password/CheckYourEmailFragment.kt
@@ -1,13 +1,19 @@
 package com.aaonri.app.ui.authentication.forgot_password
 
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.aaonri.app.R
 import com.aaonri.app.databinding.FragmentCheckYourEmailBinding
+import com.google.android.material.snackbar.Snackbar
+import java.lang.Exception
+
 
 class CheckYourEmailFragment : Fragment() {
     var checkYourEmailBinding: FragmentCheckYourEmailBinding? = null
@@ -18,8 +24,23 @@ class CheckYourEmailFragment : Fragment() {
         checkYourEmailBinding = FragmentCheckYourEmailBinding.inflate(inflater, container, false)
 
         checkYourEmailBinding?.apply {
-            openEmailAppBtn.setOnClickListener {
+            skipForNowTv.setOnClickListener {
                 findNavController().navigate(R.id.action_checkYourEmailFragment_to_resetPasswordInfoFragment)
+            }
+            openEmailAppBtn.setOnClickListener {
+                try {
+                    val intent = Intent(Intent.ACTION_MAIN)
+                    intent.addCategory(Intent.CATEGORY_APP_EMAIL)
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    context?.startActivity(intent)
+                } catch (e: Exception) {
+                    activity?.let { it1 ->
+                        Snackbar.make(
+                            it1.findViewById(android.R.id.content),
+                            "Gmail app is not installed", Snackbar.LENGTH_LONG
+                        ).show()
+                    }
+                }
             }
         }
 
