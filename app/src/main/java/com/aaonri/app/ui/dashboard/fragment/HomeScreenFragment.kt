4e23a20b574a2f6d639a605c4fea2085b76c0c57
shadab <shadab.saif@novostack.com>
2022-06-08 16:05:05 +0530

    adding alert dialog for log out

--- app/src/main/java/com/aaonri/app/ui/dashboard/fragment/HomeScreenFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/dashboard/fragment/HomeScreenFragment.kt
@@ -1,11 +1,14 @@
 package com.aaonri.app.ui.dashboard.fragment
 
+import android.app.AlertDialog
+import android.app.Dialog
 import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.aaonri.app.databinding.FragmentHomeScreenBinding
 import com.aaonri.app.ui.authentication.LoginActivity
 
@@ -20,9 +23,19 @@ class HomeScreenFragment : Fragment() {
 
         homeScreenBinding?.apply {
             logOutBtn.setOnClickListener {
-                val intent = Intent(requireContext(), LoginActivity::class.java)
-                startActivity(intent)
-                activity?.finish()
+                val builder = AlertDialog.Builder(context)
+                builder.setTitle("Confirm")
+                builder.setMessage("Are you sure you want to Logout")
+                builder.setPositiveButton("OK") { dialog, which ->
+                    val intent = Intent(requireContext(), LoginActivity::class.java)
+                    startActivity(intent)
+                    activity?.finish()
+                }
+                builder.setNegativeButton("Cancel") { dialog, which ->
+
+                }
+                builder.setIcon(android.R.drawable.ic_dialog_alert)
+                builder.show()
             }
         }
 
