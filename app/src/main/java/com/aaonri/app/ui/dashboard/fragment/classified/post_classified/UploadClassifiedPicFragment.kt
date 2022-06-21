ac6c0136d650a1b9ace9087753c4450e87b4746f
shadab <shadab.saif@novostack.com>
2022-06-21 15:17:56 +0530

    adding border to the upload pics

--- app/src/main/java/com/aaonri/app/ui/dashboard/fragment/classified/post_classified/UploadClassifiedPicFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/dashboard/fragment/classified/post_classified/UploadClassifiedPicFragment.kt
@@ -3,7 +3,6 @@ package com.aaonri.app.ui.dashboard.fragment.classified.post_classified
 import android.app.Activity
 import android.net.Uri
 import android.os.Bundle
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -21,7 +20,6 @@ import com.aaonri.app.data.classified.viewmodel.PostClassifiedViewModel
 import com.aaonri.app.databinding.FragmentUploadClassifiedPicBinding
 import com.github.dhaval2404.imagepicker.ImagePicker
 import dagger.hilt.android.AndroidEntryPoint
-import kotlin.math.log
 
 @AndroidEntryPoint
 class UploadClassifiedPicFragment : Fragment() {
@@ -56,28 +54,63 @@ class UploadClassifiedPicFragment : Fragment() {
                 }
             }
 
-                uploadedImage1.setOnClickListener {
-                    if (listOfImagesUri[0].toString().isNotEmpty()){
-                        selectedImage.load(listOfImagesUri[0])
-                    }
-                }
-                uploadedImage2.setOnClickListener {
-                    if (listOfImagesUri[1].toString().isNotEmpty()){
-                        selectedImage.load(listOfImagesUri[1])
-                    }
-                }
-                uploadedImage3.setOnClickListener {
-                    if (listOfImagesUri[2].toString().isNotEmpty()){
-                        selectedImage.load(listOfImagesUri[2])
-                    }
-                }
-                uploadedImage4.setOnClickListener {
-                    if (listOfImagesUri[3].toString().isNotEmpty()){
-                        selectedImage.load(listOfImagesUri[3])
-                    }
-                }
+            deleteImage1.setOnClickListener {
+                deleteImage(index = 0)
+            }
+            deleteImage2.setOnClickListener {
+                deleteImage(index = 1)
+            }
+            deleteImage3.setOnClickListener {
+                deleteImage(index = 2)
+            }
+            deleteImage4.setOnClickListener {
+                deleteImage(index = 3)
+            }
 
 
+
+            uploadedImage1.setOnClickListener {
+                listOfImagesUri.forEachIndexed { index, _ ->
+                    if (index == 0) changeCardViewBg(index)
+                }
+            }
+            uploadedImage2.setOnClickListener {
+                listOfImagesUri.forEachIndexed { index, uri ->
+                    if (index == 1) changeCardViewBg(index)
+                }
+            }
+            uploadedImage3.setOnClickListener {
+                listOfImagesUri.forEachIndexed { index, uri ->
+                    if (index == 2) changeCardViewBg(index)
+                }
+            }
+            uploadedImage4.setOnClickListener {
+                listOfImagesUri.forEachIndexed { index, uri ->
+                    if (index == 3) changeCardViewBg(index)
+                }
+            }
+
+
+            /*uploadedImage1.setOnClickListener {
+                if (listOfImagesUri.size == 1){
+                    selectedImage.load(listOfImagesUri[0])
+                }
+            }
+            uploadedImage2.setOnClickListener {
+                if (listOfImagesUri.size == 2){
+                    selectedImage.load(listOfImagesUri[1])
+                }
+            }
+            uploadedImage3.setOnClickListener {
+                if (listOfImagesUri.size == 3){
+                    selectedImage.load(listOfImagesUri[2])
+                }
+            }
+            uploadedImage4.setOnClickListener {
+                if (listOfImagesUri.size == 4){
+                    selectedImage.load(listOfImagesUri[3])
+                }
+            }*/
             classifiedUploadPicNextBtn.setOnClickListener {
                 findNavController().navigate(R.id.action_uploadClassifiedPicFragment_to_addressDetailsClassifiedFragment)
             }
@@ -92,14 +125,13 @@ class UploadClassifiedPicFragment : Fragment() {
             val data = result.data
 
             if (resultCode == Activity.RESULT_OK) {
-                //Image Uri will not be null for RESULT_OK
                 val fileUri = data?.data!!
 
                 uploadClassifiedBinding?.selectedImage?.setImageURI(fileUri)
 
                 listOfImagesUri.add(fileUri)
 
-                setImage(listOfImagesUri)
+                setImage()
 
                 if (listOfImagesUri.size < 4) {
                     uploadClassifiedBinding?.uploadPicBtn?.setImageDrawable(context?.let { it1 ->
@@ -122,19 +154,235 @@ class UploadClassifiedPicFragment : Fragment() {
             }
         }
 
-    private fun setImage(listOfImagesUri: MutableList<Uri>) {
+    private fun setImage() {
         if (image1 && listOfImagesUri[0].toString().isNotEmpty()) {
             uploadClassifiedBinding?.uploadedImage1?.setImageURI(listOfImagesUri[0])
             image1 = false
+            uploadClassifiedBinding?.deleteImage1?.visibility = View.VISIBLE
         } else if (image2 && listOfImagesUri[1].toString().isNotEmpty()) {
             uploadClassifiedBinding?.uploadedImage2?.setImageURI(listOfImagesUri[1])
             image2 = false
+            uploadClassifiedBinding?.deleteImage2?.visibility = View.VISIBLE
         } else if (image3 && listOfImagesUri[2].toString().isNotEmpty()) {
             uploadClassifiedBinding?.uploadedImage3?.setImageURI(listOfImagesUri[2])
             image3 = false
+            uploadClassifiedBinding?.deleteImage3?.visibility = View.VISIBLE
         } else if (image4 && listOfImagesUri[3].toString().isNotEmpty()) {
             uploadClassifiedBinding?.uploadedImage4?.setImageURI(listOfImagesUri[3])
             image4 = false
+            uploadClassifiedBinding?.deleteImage4?.visibility = View.VISIBLE
+        }
+
+    }
+
+    private fun deleteImage(index: Int) {
+        if (listOfImagesUri[index].toString().isNotEmpty() && index == 0) {
+            uploadClassifiedBinding?.uploadedImage1?.load(R.drawable.ic_uplaoded_image)
+            uploadClassifiedBinding?.deleteImage1?.visibility = View.GONE
+            listOfImagesUri.removeAt(index)
+            image1 = true
+        } else if (listOfImagesUri[index].toString().isNotEmpty() && index == 1) {
+            uploadClassifiedBinding?.uploadedImage2?.load(R.drawable.ic_uplaoded_image)
+            uploadClassifiedBinding?.deleteImage2?.visibility = View.GONE
+            listOfImagesUri.removeAt(index)
+            image2 = true
+        } else if (listOfImagesUri[index].toString().isNotEmpty() && index == 2) {
+            uploadClassifiedBinding?.uploadedImage3?.load(R.drawable.ic_uplaoded_image)
+            uploadClassifiedBinding?.deleteImage3?.visibility = View.GONE
+            listOfImagesUri.removeAt(index)
+            image3 = true
+        } else if (listOfImagesUri[index].toString().isNotEmpty() && index == 3) {
+            uploadClassifiedBinding?.uploadedImage4?.load(R.drawable.ic_uplaoded_image)
+            uploadClassifiedBinding?.deleteImage4?.visibility = View.GONE
+            listOfImagesUri.removeAt(index)
+            image4 = true
+        }
+
+        if (listOfImagesUri.size < 4) {
+            uploadClassifiedBinding?.uploadPicBtn?.setImageDrawable(context?.let { it1 ->
+                ContextCompat.getDrawable(
+                    it1, R.drawable.ic_add_image_icon
+                )
+            })
+        } else {
+            uploadClassifiedBinding?.uploadPicBtn?.setImageDrawable(context?.let { it1 ->
+                ContextCompat.getDrawable(
+                    it1, R.drawable.ic_disabled_add_btn
+                )
+            })
+        }
+    }
+
+    private fun changeCardViewBg(selectedImageIndex: Int) {
+
+        if (selectedImageIndex == 0) {
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.selectedClassifiedCardViewBorder
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage1?.setBackgroundColor(
+                    it2
+                )
+            }
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage2?.setBackgroundColor(
+                    it2
+                )
+            }
+
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage3?.setBackgroundColor(
+                    it2
+                )
+            }
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage4?.setBackgroundColor(
+                    it2
+                )
+            }
+        } else if (selectedImageIndex == 1) {
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage1?.setBackgroundColor(
+                    it2
+                )
+            }
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.selectedClassifiedCardViewBorder
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage2?.setBackgroundColor(
+                    it2
+                )
+            }
+
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage3?.setBackgroundColor(
+                    it2
+                )
+            }
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage4?.setBackgroundColor(
+                    it2
+                )
+            }
+        } else if (selectedImageIndex == 2) {
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage1?.setBackgroundColor(
+                    it2
+                )
+            }
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage2?.setBackgroundColor(
+                    it2
+                )
+            }
+
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.selectedClassifiedCardViewBorder
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage3?.setBackgroundColor(
+                    it2
+                )
+            }
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage4?.setBackgroundColor(
+                    it2
+                )
+            }
+        } else if (selectedImageIndex == 3) {
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage1?.setBackgroundColor(
+                    it2
+                )
+            }
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage2?.setBackgroundColor(
+                    it2
+                )
+            }
+
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.white
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage3?.setBackgroundColor(
+                    it2
+                )
+            }
+            context?.let { it1 ->
+                ContextCompat.getColor(
+                    it1,
+                    R.color.selectedClassifiedCardViewBorder
+                )
+            }?.let { it2 ->
+                uploadClassifiedBinding?.uploadedImage4?.setBackgroundColor(
+                    it2
+                )
+            }
         }
     }
 }
