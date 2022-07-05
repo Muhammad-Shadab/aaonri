81229042b7a781d478a46e9b4e081fd00b3bf96c
Dipanshu <dipanshu.singh@novostack.com>
2022-07-05 12:34:20 +0530

    all event screen details

--- app/src/main/java/com/aaonri/app/data/event/adapter/AllEventAdapter.kt
+++ app/src/main/java/com/aaonri/app/data/event/adapter/AllEventAdapter.kt
@@ -1,18 +1,27 @@
 package com.aaonri.app.data.event.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.aaonri.app.data.event.model.Event
-import com.aaonri.app.data.event.model.RecentEventResponse
 import com.aaonri.app.data.event.model.RecentEventResponseItem
 import com.aaonri.app.databinding.EventItemBinding
 import com.bumptech.glide.Glide
+import java.time.LocalTime
+import java.time.format.DateTimeFormatter
+import java.util.*
 
 class AllEventAdapter(private var selectedServices: ((value: Event) -> Unit)) :
     RecyclerView.Adapter<AllEventAdapter.EventViewHolder>() {
 
     private var data = listOf<Event>()
+    private var startDate:String?=null
+    private var startTimeOfEvent:String?=null
+    private var endTimeOfEvent:String?=null
+    private var timeZone:String?=null
+
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): EventViewHolder {
         val inflater = LayoutInflater.from(parent.context)
@@ -20,6 +29,7 @@ class AllEventAdapter(private var selectedServices: ((value: Event) -> Unit)) :
         return EventViewHolder(binding)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: EventViewHolder, position: Int) {
         val context = holder.itemView.context
         holder.binding.apply {
@@ -29,6 +39,13 @@ class AllEventAdapter(private var selectedServices: ((value: Event) -> Unit)) :
                 Glide.with(context).load(image)
                     .into(eventImageView)
                 eventName.text = data[position].title
+
+                startDate = DateTimeFormatter.ofPattern("MMM dd").format(DateTimeFormatter.ofPattern("yyyy-MM-dd").parse(data[position].startDate.split("T")[0]))
+                startTimeOfEvent=LocalTime.parse(data[position].startTime).format(DateTimeFormatter.ofPattern("h:mma"))
+                endTimeOfEvent=LocalTime.parse(data[position].endTime).format(DateTimeFormatter.ofPattern("h:mma"))
+                timeZone=data[position].timeZone
+
+                eventTiming.text= "$startDate, $startTimeOfEvent - $endTimeOfEvent  $timeZone"
                 totalVisiting.text = data[position].totalVisiting.toString()
                 totalFavourite.text = data[position].totalFavourite.toString()
                 eventLocationZip.text = data[position].city + "-" + data[position].zipCode
