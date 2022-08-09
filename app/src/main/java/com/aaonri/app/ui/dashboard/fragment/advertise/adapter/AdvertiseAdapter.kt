f8e1b4e247f62773128eb80cea52a85b9e95436e
Dipanshu <dipanshu.singh@novostack.com>
2022-08-09 18:01:26 +0530

    adding advertisementDetails Data

--- app/src/main/java/com/aaonri/app/ui/dashboard/fragment/advertise/adapter/AdvertiseAdapter.kt
+++ app/src/main/java/com/aaonri/app/ui/dashboard/fragment/advertise/adapter/AdvertiseAdapter.kt
@@ -1,12 +1,16 @@
 package com.aaonri.app.ui.dashboard.fragment.advertise.adapter
 
+import android.os.Build
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
+import com.aaonri.app.data.advertise.model.AdvertiseDetailsResponse
 import com.aaonri.app.data.advertise.model.AllAdvertiseResponse
 import com.aaonri.app.data.advertise.model.AllAdvertiseResponseItem
 import com.aaonri.app.databinding.AdvertisementItemBinding
 import com.bumptech.glide.Glide
+import java.time.format.DateTimeFormatter
 
 class AdvertiseAdapter(private var selectedServices: ((value: AllAdvertiseResponseItem) -> Unit)) :
     RecyclerView.Adapter<AdvertiseAdapter.AdvertiseViewHolder>() {
@@ -19,11 +23,19 @@ class AdvertiseAdapter(private var selectedServices: ((value: AllAdvertiseRespon
         return AdvertiseViewHolder(binding)
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     override fun onBindViewHolder(holder: AdvertiseViewHolder, position: Int) {
         val context = holder.itemView.context
         holder.binding.apply {
             advertiseNameTv.text = data[position].title
-            advertiseLocationTv.text = data[position].fromDate
+//            advertiseLocationTv.text  = data[position].advertisementDetails?.location
+            advertiseDateTv.text = "From ${DateTimeFormatter.ofPattern("MMM dd,yyyy").format(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                    .parse(data[position]?.fromDate?.split("T")?.get(0))
+            ) } To ${DateTimeFormatter.ofPattern("MMM dd,yyyy").format(
+                DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                    .parse(data[position]?.toDate?.split("T")?.get(0))
+            ) }"
 
             //Glide.with(context).load(advertisemntImage).into(advertisemntImage)
         }
@@ -33,7 +45,7 @@ class AdvertiseAdapter(private var selectedServices: ((value: AllAdvertiseRespon
     }
 
     @JvmName("setData1")
-    fun setData(data: List<AllAdvertiseResponseItem>) {
+    fun setData(data: MutableList<AllAdvertiseResponseItem>) {
         this.data = data
         notifyDataSetChanged()
     }
--- app/src/main/java/com/aaonri/app/ui/dashboard/fragment/advertise/post_advertisement/AdvertisementDetailsFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/dashboard/fragment/advertise/post_advertisement/AdvertisementDetailsFragment.kt
@@ -1,19 +1,24 @@
 package com.aaonri.app.ui.dashboard.fragment.advertise.post_advertisement
 
+import android.os.Build
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.navArgs
+import com.aaonri.app.BuildConfig
 import com.aaonri.app.R
 import com.aaonri.app.data.advertise.model.AdvertiseDetailsResponse
 import com.aaonri.app.data.advertise.viewmodel.AdvertiseViewModel
 import com.aaonri.app.databinding.FragmentAdvertisementDetailsBinding
 import com.aaonri.app.utils.Resource
+import com.bumptech.glide.Glide
 import dagger.hilt.android.AndroidEntryPoint
+import java.time.format.DateTimeFormatter
 
 @AndroidEntryPoint
 class AdvertisementDetailsFragment : Fragment() {
@@ -51,7 +56,47 @@ class AdvertisementDetailsFragment : Fragment() {
         return detailsBinding?.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
     private fun setData(data: AdvertiseDetailsResponse?) {
+        detailsBinding?.addImage?.let {
+            context?.let { it1 ->
+                Glide.with(it1)
+                    .load("${BuildConfig.BASE_URL}/api/v1/common/advertisementFile/${data?.advertisementDetails?.adImage}")
+                    .into(it)
+            }
+
+        }
+        detailsBinding?.advertiseNameTv?.text =  data?.advertisementDetails?.adTitle
+        detailsBinding?.advertiseDateTv?.text = "From ${DateTimeFormatter.ofPattern("MMM dd,yyyy").format(
+            DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                .parse(data?.fromDate?.split("T")?.get(0))
+        ) } To ${DateTimeFormatter.ofPattern("MMM dd,yyyy").format(
+            DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                .parse(data?.toDate?.split("T")?.get(0))
+        ) }"
+        detailsBinding?.advertiseLocationTv?.text = data?.advertisementDetails?.location
+        detailsBinding?.advertiseLinkTv?.text = data?.advertisementDetails?.url
+        detailsBinding?.companyDescTv?.text = data?.advertisementDetails?.companyDescription
+        detailsBinding?.companyNameTv?.text = data?.advertisementDetails?.companyName
+//        detailsBinding?.companyLocationTv?.text = data?.advertisementDetails?.
+        detailsBinding?.companyContactTv?.text = data?.advertisementDetails?.contactNo
+        detailsBinding?.companyEmailTv?.text = data?.advertisementDetails?.emailId
+        detailsBinding?.companyServicesTv?.text = data?.advertisementDetails?.productServices
+        detailsBinding?.companyUriTv?.text = data?.advertisementDetails?.url
+        detailsBinding?.companyStartDateTv?.text = DateTimeFormatter.ofPattern("MMM dd,yyyy").format(
+            DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                .parse(data?.fromDate?.split("T")?.get(0))
+        )
+        detailsBinding?.companyEndDateTv?.text = DateTimeFormatter.ofPattern("MMM dd,yyyy").format(
+            DateTimeFormatter.ofPattern("yyyy-MM-dd")
+                .parse(data?.toDate?.split("T")?.get(0))
+        )
+        detailsBinding?.companyPlanTv?.text = data?.locationPlanRate?.days.toString()
+        detailsBinding?.companyVasTv?.text = data?.advertisementVasMap.toString()
+        detailsBinding?.companyAdTitleTv?.text = data?.advertisementDetails?.adTitle
+        detailsBinding?.companyTemplateTv?.text = data?.template?.name
+        detailsBinding?.companyAdPageTv?.text = data?.advertisementPageLocation?.advertisementPage?.pageName
+        detailsBinding?.companyPageLocationNameTv?.text = data?.advertisementPageLocation?.locationName
 
     }
 
