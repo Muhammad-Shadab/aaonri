2429b6c24d6dadf5d9c7553d5213c80f4c03e3fb
shadab <shadab.saif@novostack.com>
2022-08-17 20:46:37 +0530

    adding preview advertise

--- app/src/main/java/com/aaonri/app/ui/dashboard/fragment/advertise/fragment/ReviewAdvertiseFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/dashboard/fragment/advertise/fragment/ReviewAdvertiseFragment.kt
@@ -5,20 +5,45 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.aaonri.app.R
+import com.aaonri.app.data.advertise.AdvertiseConstant
+import com.aaonri.app.data.advertise.viewmodel.PostAdvertiseViewModel
+import com.aaonri.app.data.classified.viewmodel.PostClassifiedViewModel
 import com.aaonri.app.databinding.FragmentReviewAdvertiseBinding
+import com.bumptech.glide.Glide
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import dagger.hilt.android.AndroidEntryPoint
 
-class ReviewAdvertiseFragment  : BottomSheetDialogFragment() {
+@AndroidEntryPoint
+class ReviewAdvertiseFragment : BottomSheetDialogFragment() {
     override fun getTheme(): Int = R.style.BottomSheetDialogTheme
-  var reviewBinding : FragmentReviewAdvertiseBinding? = null
+    var reviewBinding: FragmentReviewAdvertiseBinding? = null
+    val postClassifiedViewModel: PostAdvertiseViewModel by activityViewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        reviewBinding =  FragmentReviewAdvertiseBinding.inflate(inflater, container, false)
+        isCancelable = false
+        reviewBinding = FragmentReviewAdvertiseBinding.inflate(inflater, container, false)
+
+        reviewBinding?.apply {
+
+            closeCommunityBtn.setOnClickListener {
+                findNavController().navigateUp()
+            }
+
+            context?.let {
+                Glide.with(it)
+                    .load(postClassifiedViewModel.companyBasicDetailsMap[AdvertiseConstant.ADVERTISE_IMAGE_URI])
+                    .into(advertisementImage)
+            }
+
+
+        }
+
         return reviewBinding?.root
     }
 
