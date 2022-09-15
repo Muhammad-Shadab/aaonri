04bd282c7f1744b3e3ad3e3d84480e2f0935e04d
Shadab <shadab.saif@novostack.com>
2022-09-15 15:10:57 +0530

    adding storage permission

--- app/src/main/java/com/aaonri/app/ui/dashboard/fragment/jobs/post_jobs/JobProfileUploadFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/dashboard/fragment/jobs/post_jobs/JobProfileUploadFragment.kt
@@ -1,20 +1,57 @@
 package com.aaonri.app.ui.dashboard.fragment.jobs.post_jobs
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
 import com.aaonri.app.data.jobs.seeker.viewmodel.JobSeekerViewModel
 import com.aaonri.app.databinding.FragmentUploadJobProfileBinding
+import com.aaonri.app.utils.Validator
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
+
 
 @AndroidEntryPoint
 class JobProfileUploadFragment : Fragment() {
     var binding: FragmentUploadJobProfileBinding? = null
     val jobSeekerViewModel: JobSeekerViewModel by activityViewModels()
+
+    private val resultLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+                if (data != null) {
+                    val pdfUri = data.data
+                    Toast.makeText(context, "uri $pdfUri", Toast.LENGTH_SHORT).show()
+                    val path = pdfUri?.path
+                    Toast.makeText(context, "path $path", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
+    val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            val intent = Intent(Intent.ACTION_GET_CONTENT)
+            intent.type = "application/pdf"
+            resultLauncher.launch(intent)
+        } else {
+            showAlert("Please allow storage permission")
+        }
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -22,6 +59,8 @@ class JobProfileUploadFragment : Fragment() {
         binding = FragmentUploadJobProfileBinding.inflate(layoutInflater, container, false)
 
         binding?.apply {
+
+            uploadResumeLl.gravity = Gravity.CENTER
 
             navigateBack.setOnClickListener {
                 findNavController().navigateUp()
@@ -50,6 +89,67 @@ class JobProfileUploadFragment : Fragment() {
                     )
                 findNavController().navigate(action)
             }
+
+            uploadResumeLl.setOnClickListener {
+                if (checkPermission()) {
+                    val intent = Intent(Intent.ACTION_GET_CONTENT);
+                    intent.type = "application/pdf";
+                    resultLauncher.launch(intent)
+                } else {
+                    requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }
+
+            uploadProfileNextBtn.setOnClickListener {
+                if (firstNameEt.text.toString().length >= 3) {
+                    if (lastNameEt.text.toString().length >= 3) {
+                        if (currentTitleEt.text.toString().length >= 3) {
+                            if (Validator.emailValidation(contactEmailEt.text.toString().trim())) {
+                                if (phoneNumberEt.text.toString().length == 10) {
+                                    if (locationEt.text.toString().length >= 3) {
+                                        if (selectExperienceTv.text.toString().isNotEmpty()) {
+                                            if (selectVisaStatusTv.text.toString().isNotEmpty()) {
+                                                if (selectAvailabilityTv.text.toString()
+                                                        .isNotEmpty()
+                                                ) {
+                                                    if (skillSetDescEt.text.toString().length >= 3) {
+                                                        if (coverLetterDescEt.text.toString().length >= 3) {
+
+                                                        } else {
+                                                            showAlert("Please enter valid Cover Letter Description")
+                                                        }
+                                                    } else {
+                                                        showAlert("Please enter valid Skill Set")
+                                                    }
+                                                } else {
+                                                    showAlert("Please choose valid Availability")
+                                                }
+                                            } else {
+                                                showAlert("Please choose valid Visa Status")
+                                            }
+                                        } else {
+                                            showAlert("Please choose valid Experience")
+                                        }
+                                    } else {
+                                        showAlert("Please enter valid Location")
+                                    }
+                                } else {
+                                    showAlert("Please enter valid Phone Number")
+                                }
+                            } else {
+                                showAlert("Please enter valid Email")
+                            }
+                        } else {
+                            showAlert("Please enter valid Title")
+                        }
+                    } else {
+                        showAlert("Please enter valid Last Name")
+                    }
+                } else {
+                    showAlert("Please enter valid First Name")
+                }
+            }
+
         }
 
         jobSeekerViewModel.getAllActiveJobApplicability()
@@ -73,6 +173,24 @@ class JobProfileUploadFragment : Fragment() {
         }
 
         return binding?.root
+    }
+
+    private fun showAlert(text: String) {
+        activity?.let { it1 ->
+            Snackbar.make(
+                it1.findViewById(android.R.id.content),
+                text, Snackbar.LENGTH_LONG
+            ).show()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return context?.let {
+            ContextCompat.checkSelfPermission(
+                it,
+                Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        } == PackageManager.PERMISSION_GRANTED
     }
 
     override fun onDestroy() {
