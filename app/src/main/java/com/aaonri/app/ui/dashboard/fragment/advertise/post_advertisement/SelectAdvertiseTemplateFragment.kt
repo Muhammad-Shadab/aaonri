f284d56c4941d0c81d86e7d90c6b1c8182d994d7
Dipanshu <dipanshu.singh@novostack.com>
2022-09-26 18:27:45 +0530

    added json data on advertisement template  screen

--- app/src/main/java/com/aaonri/app/ui/dashboard/fragment/advertise/post_advertisement/SelectAdvertiseTemplateFragment.kt
+++ app/src/main/java/com/aaonri/app/ui/dashboard/fragment/advertise/post_advertisement/SelectAdvertiseTemplateFragment.kt
@@ -11,13 +11,19 @@ import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.aaonri.app.R
 import com.aaonri.app.data.advertise.AdvertiseConstant
+import com.aaonri.app.data.advertise.model.AdvertiseActivePageResponseItem
 import com.aaonri.app.data.advertise.viewmodel.PostAdvertiseViewModel
+import com.aaonri.app.data.immigration.model.ImmigrationCenterModelItem
 import com.aaonri.app.databinding.FragmentSelectAdvertiseTemplateBinding
 import com.aaonri.app.ui.dashboard.fragment.advertise.adapter.AdvertiseTemplateAdapter
 import com.aaonri.app.utils.PreferenceManager
 import com.aaonri.app.utils.Resource
 import com.google.android.material.snackbar.Snackbar
+import com.google.gson.Gson
 import dagger.hilt.android.AndroidEntryPoint
+import org.json.JSONObject
+import java.io.IOException
+import java.nio.charset.Charset
 
 @AndroidEntryPoint
 class SelectAdvertiseTemplateFragment : Fragment() {
@@ -25,7 +31,7 @@ class SelectAdvertiseTemplateFragment : Fragment() {
     val postAdvertiseViewModel: PostAdvertiseViewModel by activityViewModels()
 
     var advertiseTemplateAdapter1: AdvertiseTemplateAdapter? = null
-
+    var advertiseTemplateList = mutableListOf<AdvertiseActivePageResponseItem>()
     //var advertiseTemplateAdapter2: AdvertiseTemplateAdapter? = null
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -38,7 +44,7 @@ class SelectAdvertiseTemplateFragment : Fragment() {
             binding?.advertiseTemplatesNextBtn?.backgroundTintList =
                 ColorStateList.valueOf(resources.getColor(R.color.greenBtnColor))
             postAdvertiseViewModel.setTemplatePageName(it)
-            binding?.pageNameTv?.text = it.pageName
+            binding?.pageNameTv?.text = it.title
             binding?.selectedPageDescTv?.text = it.description
             binding?.pageNameTv?.visibility = View.VISIBLE
             binding?.selectedPageDescTv?.visibility = View.VISIBLE
@@ -51,7 +57,8 @@ class SelectAdvertiseTemplateFragment : Fragment() {
         binding?.apply {
 
             postAdvertiseViewModel.setNavigationForStepper(AdvertiseConstant.ADVERTISE_TEMPLATE)
-
+            val userArray = JSONObject(loadJSONFromAsset()).getJSONArray("AdvertisePageInfo")
+            val gson = Gson()
             advertiseTemplatesNextBtn.setOnClickListener {
                 if (context?.let { PreferenceManager<Int>(it)["selectedTemplatePage", -1] } != -1) {
                     val action =
@@ -67,6 +74,20 @@ class SelectAdvertiseTemplateFragment : Fragment() {
                 }
             }
 
+            for (i in 0 until userArray.length()) {
+                if(!advertiseTemplateList.contains( gson.fromJson(
+                        userArray.getString(i),
+                        AdvertiseActivePageResponseItem::class.java
+                    ))) {
+                    advertiseTemplateList.add(
+                        gson.fromJson(
+                            userArray.getString(i),
+                            AdvertiseActivePageResponseItem::class.java
+                        )
+                    )
+                }
+            }
+            advertiseTemplateAdapter1?.setData(advertiseTemplateList)
             horizontalRv1.layoutManager =
                 LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
 
@@ -74,21 +95,6 @@ class SelectAdvertiseTemplateFragment : Fragment() {
 
         }
 
-        postAdvertiseViewModel.advertisePageData.observe(viewLifecycleOwner) { response ->
-            when (response) {
-                is Resource.Loading -> {
-                    binding?.progressBar?.visibility = View.VISIBLE
-                }
-                is Resource.Success -> {
-                    binding?.progressBar?.visibility = View.GONE
-                    response.data?.let { data -> advertiseTemplateAdapter1?.setData(data) }
-                }
-                is Resource.Error -> {
-                    binding?.progressBar?.visibility = View.GONE
-                }
-                else -> {}
-            }
-        }
 
         return binding?.root
     }
@@ -97,4 +103,23 @@ class SelectAdvertiseTemplateFragment : Fragment() {
         super.onDestroyView()
         binding = null
     }
+
+
+    private fun loadJSONFromAsset(): String? {
+        val json: String?
+        try {
+            val inputStream = context?.assets?.open("pageid.json")
+            val size = inputStream?.available()
+            val buffer = size?.let { ByteArray(it) }
+            val charset: Charset = Charsets.UTF_8
+            inputStream?.read(buffer)
+            inputStream?.close()
+            json = buffer?.let { String(it, charset) }
+        } catch (ex: IOException) {
+            ex.printStackTrace()
+            return ""
+        }
+        return json
+    }
+
 }
